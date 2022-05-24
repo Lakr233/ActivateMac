af18c6d51f7ece3637256be27d3d0d0db5f9a89d
pan93412 <pan93412@gmail.com>
2022-05-25 00:04:17 +0800

    l10n: add zh-Hant translation

--- Activate/main.m
+++ Activate/main.m
@@ -145,6 +145,8 @@
     NSString *dLanguage = [languages objectAtIndex:0];
     if ([dLanguage isEqualToString:@"zh-Hans"] || [dLanguage isEqualToString:@"zh-Hans-CN"]) {
         return @[@"激活 macOS", @"您当前所使用的可能是盗版 macOS 副本，请前往偏好设置激活。"];
+    } else if ([dLanguage isEqualToString:@"zh-Hant"] || [dLanguage isEqualToString:@"zh-Hant-TW"]) {
+        return @[@"啟用 macOS", @"您目前使用的可能是盜版 macOS 副本。請前往「系統偏好設定」啟用。"];
     } else if ([dLanguage isEqualToString:@"ja-JP"]) {
         return @[@"macOS をアクティブ化", @"「システム環境設定」アクティブ化に行ってください。"];
     } else {
